b8dcfa37ed701cc2d0fb35fd320a6af8d6b36d71
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-10-03 10:38:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("hello Nassime1"))
+console.log(chalk.red("hello NassimeOfficial"))
+console.log(chalk.green("hello Nasssime l'unique"))
+console.log(chalk.grey("hello Nas"))
